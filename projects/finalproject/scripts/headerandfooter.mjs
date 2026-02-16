d74d275d19cca08b08529869b07530502c121f36
vamundse <vamundse@gmail.com>
2026-02-16 08:25:38 +0100

    Made a dynamic header and footer so that I can make quick changes

--- projects/finalproject/scripts/headerandfooter.mjs
+++ projects/finalproject/scripts/headerandfooter.mjs
@@ -1,4 +1,13 @@
-const header = document.querySelector('header');
-const headerFetch = fetch('./header.html');
-const headerTemplate = headerFetch.text();
-header.insertAdjacentHTML('beforeend', headerTemplate);
+export async function header() {
+    const header = document.querySelector('header');
+    const headerFetch = await fetch('./header.html');
+    const headerTemplate = await headerFetch.text();
+    header.insertAdjacentHTML('beforeend', headerTemplate);
+}
+
+export async function footer() {
+    const footer = document.querySelector('footer');
+    const footerFetch = await fetch('./footer.html');
+    const footerTemplate = await footerFetch.text();
+    footer.insertAdjacentHTML('beforeend', footerTemplate);
+} 
