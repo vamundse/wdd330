c4e75105ef06c884350dbb72a7367e3e82f11de9
vamundse <vamundse@gmail.com>
2026-02-16 09:03:42 +0100

    Tried some url meta stuff

--- projects/finalproject/scripts/headerandfooter.mjs
+++ projects/finalproject/scripts/headerandfooter.mjs
@@ -1,13 +1,13 @@
 export async function header() {
     const header = document.querySelector('header');
-    const headerFetch = await fetch('header.html');
+    const headerFetch = await fetch(new URL('../header.html', import.meta.url));;
     const headerTemplate = await headerFetch.text();
     header.innerHTML = headerTemplate;
 }
 
 export async function footer() {
     const footer = document.querySelector('footer');
-    const footerFetch = await fetch('footer.html');
+    const footerFetch = await fetch(new URL('../footer.html', import.meta.url));
     const footerTemplate = await footerFetch.text();
     footer.innerHTML = footerTemplate;
 }
