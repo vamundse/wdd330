096502663e7c143950832b47260e6d45a38d0c04
vamundse <vamundse@gmail.com>
2026-02-16 08:58:41 +0100

    Changed to innerHTML to see if it makes any difference

--- projects/finalproject/scripts/headerandfooter.mjs
+++ projects/finalproject/scripts/headerandfooter.mjs
@@ -2,12 +2,12 @@ export async function header() {
     const header = document.querySelector('header');
     const headerFetch = await fetch('header.html');
     const headerTemplate = await headerFetch.text();
-    header.insertAdjacentHTML('beforeend', headerTemplate);
+    header.innerHTML = headerTemplate;
 }
 
 export async function footer() {
     const footer = document.querySelector('footer');
     const footerFetch = await fetch('footer.html');
     const footerTemplate = await footerFetch.text();
-    footer.insertAdjacentHTML('beforeend', footerTemplate);
+    footer.innerHTML = footerTemplate;
 }
