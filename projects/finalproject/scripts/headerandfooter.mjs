b8da1f13e8581f6cd3386f7a44a2e52603fb6bc3
vamundse <vamundse@gmail.com>
2026-02-16 08:48:37 +0100

    Changed the paths of the header and footer html files

--- projects/finalproject/scripts/headerandfooter.mjs
+++ projects/finalproject/scripts/headerandfooter.mjs
@@ -1,13 +1,13 @@
 export async function header() {
     const header = document.querySelector('header');
-    const headerFetch = await fetch('header.html');
+    const headerFetch = await fetch('../header.html');
     const headerTemplate = await headerFetch.text();
     header.insertAdjacentHTML('beforeend', headerTemplate);
 }
 
 export async function footer() {
     const footer = document.querySelector('footer');
-    const footerFetch = await fetch('footer.html');
+    const footerFetch = await fetch('../footer.html');
     const footerTemplate = await footerFetch.text();
     footer.insertAdjacentHTML('beforeend', footerTemplate);
 }
